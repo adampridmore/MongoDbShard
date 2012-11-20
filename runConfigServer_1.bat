55948c9e4abd4c9263b108453aa2b9226bda835e
Adam Pridmore <adam_pridmore@hotmail.com>
2012-11-20 16:30:43 +0000

    Added missing files.

--- runConfigServer_1.bat
+++ runConfigServer_1.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Mongo\bin\mongod.exe" --configsvr --dbpath C:\MongoShard\data\config-1 --port 27019 --logpath C:\MongoShard\data\config-1.log --nojournal
+"C:\Program Files\Mongo\bin\mongod.exe" --configsvr --dbpath C:\MongoShard\data\config-1 --port 27019 --nojournal
