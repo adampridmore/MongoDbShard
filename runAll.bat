9f123e8b2969a971207cc00dbe7aa5d7ff8ec68f
Adam Pridmore <adam_pridmore@hotmail.com>
2012-11-22 10:24:08 +0000

    Fixed invalid batch file comments.

--- runAll.bat
+++ runAll.bat
@@ -1,17 +1,17 @@
-# Run Shard A
+rem Run Shard A
 start C:\MongoShard\data\runServer_a_1.bat
 start C:\MongoShard\data\runServer_a_2.bat
 start C:\MongoShard\data\runServer_a_3.bat
 
-# Run Shard B
+rem Run Shard B
 start C:\MongoShard\data\runServer_b_1.bat
 start C:\MongoShard\data\runServer_b_2.bat
 start C:\MongoShard\data\runServer_b_3.bat
 
-# Run config servers
+rem Run config servers
 start C:\MongoShard\data\runConfigServer_1.bat
 start C:\MongoShard\data\runConfigServer_2.bat
 start C:\MongoShard\data\runConfigServer_3.bat
 
-# Run mongos
+rem Run mongos
 start C:\MongoShard\data\runMongos.bat
