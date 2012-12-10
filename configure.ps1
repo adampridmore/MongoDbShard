832c427e8c2099a3aaae8f20509dc7a37b2abc94
Adam Pridmore <adam_pridmore@hotmail.com>
2012-12-10 13:45:13 +0000

    Added script to configure
    Using powershell.
    
    Coudn't get a multi-step javascript file to work.

---
+++ configure.ps1
@@ -0,0 +1,18 @@
+& mongo.exe MNAB-DEV15L:30000 --eval "rs.initiate()"
+Start-Sleep -s 2
+& mongo.exe MNAB-DEV15L:30000 --eval "rs.add('MNAB-DEV15L:30001')"
+Start-Sleep -s 2
+& mongo.exe MNAB-DEV15L:30000 --eval "rs.addArb('MNAB-DEV15L:30002')"
+Start-Sleep -s 2
+
+& mongo.exe MNAB-DEV15L:30100 --eval "rs.initiate()"
+Start-Sleep -s 2
+& mongo.exe MNAB-DEV15L:30100 --eval "rs.add('MNAB-DEV15L:30101')"
+Start-Sleep -s 2
+& mongo.exe MNAB-DEV15L:30100 --eval "rs.addArb('MNAB-DEV15L:30102')"
+Start-Sleep -s 2
+
+& mongo.exe MNAB-DEV15L:40000 --eval "sh.addShard('shard-a/MNAB-DEV15L:30000,MNAB-DEV15L:30001')"
+Start-Sleep -s 2
+& mongo.exe MNAB-DEV15L:40000 --eval "sh.addShard('shard-b/MNAB-DEV15L:30100,MNAB-DEV15L:30101')"
+Start-Sleep -s 2
